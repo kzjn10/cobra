c5ddc9297645fe52e038b4da0da46e729dd37cc6
Anh Nguyễn <kzjn10@users.noreply.github.com>
2025-01-24 14:54:09 +0700

    init

---
+++ src/app.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get , Res} from '@nestjs/common';
+import { AppService } from './app.service';
+import * as path from 'path';
+import { Response } from 'express';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('find-phone-policy')
+  getFindPhonePolicy(@Res() res: Response): void {
+    const filePath = path.join(__dirname, '..', 'files', 'find-phone-policy.html');
+    res.sendFile(filePath);
+  }
+
+  @Get('find-phone-term')
+  getFindPhoneTerm(@Res() res: Response): void {
+    const filePath = path.join(__dirname, '..', 'files', 'find-phone-term.html');
+    res.sendFile(filePath);
+  }
+}
